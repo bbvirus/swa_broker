912e044baf3674b8e4781bd42a84f90e9d4faf2e
parksehun <bbvirus@naver.com>
2014-04-13 20:33:24 +0900

    test commit

--- HelloClient.java
+++ HelloClient.java
@@ -8,15 +8,13 @@ public class HelloClient{
   public static void main(String args[]){
   
     try{
-      // create and initialize the ORB
+      // ORB를 생성한다.
       ORB orb = ORB.init(args, null);
 
       // get the root naming context
-      org.omg.CORBA.Object objRef = 
-          orb.resolve_initial_references("NameService");
+      org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
           
-      // Use NamingContextExt instead of NamingContext. This is 
-      // part of the Interoperable naming Service.  
+      // Use NamingContextExt instead of NamingContext. This is part of the Interoperable naming Service.
       NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
  
       // resolve the Object Reference in Naming
