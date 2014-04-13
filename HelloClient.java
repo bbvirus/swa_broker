7a44a7546d508f86a12b8179dab32309fa98e933
parksehun <bbvirus@naver.com>
2014-04-14 01:46:01 +0900

    HelloClient와 HelloServer의 동작에 대한 주석 추가

--- HelloClient.java
+++ HelloClient.java
@@ -8,21 +8,32 @@ public class HelloClient{
   public static void main(String args[]){
   
     try{
-      // ORB를 생성한다.
+      // ORB를 생성하고
+      // 이를 활용해 naming context를 활용하기 위한 준비 작업을 한다.
       ORB orb = ORB.init(args, null);
-
-      // get the root naming context
       org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
           
       // Use NamingContextExt instead of NamingContext. This is part of the Interoperable naming Service.
       NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
  
       // resolve the Object Reference in Naming
+      // "Hello"라는 이름을 가지고 naming context를 활용해 서비스를 찾는다.
+      // HelloHelper의 narrow 메서드를 호출하면
+      // Hello 인터페이스를 구현한 _HelloStub의 인스턴스가 반환된다.
+      // 반환된 인스턴스는 helloImpl에 저장된다.
       String name = "Hello";
       Hello helloImpl = HelloHelper.narrow(ncRef.resolve_str(name));
 
+      // helloImpl의 sayHello()메서드는 _HelloStub.java의 sayHello()메서드이다.
+      // _HelloStub에서 메서드 내용을 확인해보면
+      // "sayHello"라는 이름을 활용해 알맞은 서비스를 찾고
+      // 결과 String을 반환해준다. (아마도 "Hello World"일 것이다.)
       System.out.println("Obtained a handle on server object: " + helloImpl);
       System.out.println(helloImpl.sayHello());
+      
+      // helloImple에서 shutdown을 실행하면
+      // 역시 _HelloStub.java의 shutdown 메서드가 실행된다.
+      // "shutdown"이름을 가지고 리퀘스트를 처리할 서비스를 찾아 _invoke를 통해 실행시킨다.
       helloImpl.shutdown();
       }
       
@@ -32,4 +43,3 @@ public class HelloClient{
     }
   }
 }
-
--- HelloServer.java
+++ HelloServer.java
@@ -28,21 +28,27 @@ public class HelloServer{
   public static void main(String args[]){
     try{
       // create and initialize the ORB
+      // ORB를 생성하고
+      // 리퀘스트를 처리하는 데 활용할 rootpoa에 대한 참조를 얻고, POAManager를 활성화시킨다.
+      // ( POA는 객체에 대한 참조를 사용하는 리퀘스트와, 그 리퀘스트에 대한 알맞은 서비스를 제공할 코드를 연결하는 방법을 제공한다 )
       ORB orb = ORB.init(args, null);
-
-      // Get reference to rootpoa & activate the POAManager
       POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
       rootpoa.the_POAManager().activate();
 
-      // create servant and register it with the ORB
+      // HelloPOA 추상 클래스를 상속받은 helloImpl 인스턴스를 만들고
+      // ORB를 넣어둔다.
+      // HelloPOA에는 InvokeHandler와 HelloOperations가 implements 되어있다.
       HelloImpl helloImpl = new HelloImpl();
       helloImpl.setORB(orb); 
 
-      // create a tie, with servant being the delegate.
+      // HelloPOATie 객체의 인스턴스인 tie를 생성하는데
+      // 앞에서 생성한 helloImpl과 rootpoa를 생성자에 전달한다.
+      // 그러면 tie는 HelloOperations 인터페이스와 POA를 가지고 있게 된다.
       HelloPOATie tie = new HelloPOATie(helloImpl, rootpoa);
 
-      // obtain the objectRef for the tie
-      // this step also implicitly activates the object
+      // tie의 _this메서드를 실행하면 HelloHelper의 narrow 메서드가 호출된다.
+      // narrow 메서드의 반환값은 _HelloStub 클래스의 인스턴스이다.
+      // _HelloStub 클래스는 Hello 인터페이스를 구현하고 있으므로 href에 저장할 수 있다.
       Hello href = tie._this(orb);
            
       // get the root naming context
@@ -52,7 +58,7 @@ public class HelloServer{
       // Naming Service specification.
       NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
 
-      // bind the Object Reference in Naming
+      // "Hello"라는 이름을 가지고 알맞은 서비스를 찾아 바인딩해둔다.
       String name = "Hello";
       NameComponent path[] = ncRef.to_name( name );
       ncRef.rebind(path, href);
